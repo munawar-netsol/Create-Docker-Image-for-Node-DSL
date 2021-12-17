1807aad73bef83fc33d42f26adb7ffbff6d0443d
munawar-netsol <munawersheikh@gmail.com>
2021-12-17 07:05:02 +0500

    first commit

---
+++ dsl/nodejs.groovy
@@ -0,0 +1,24 @@
+job('Node.js Docker Example') {
+    scm {
+        git('git://github.com/munawar-netsol/Create-Docker-Image-for-Node-DSL.git') { node ->
+            node / gitConfigName('munawersheikh.')
+            node / gitConfigEmail('munawersheikh@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * * *')
+    }
+    wrappers {
+        nodejs('Node.js Latest')
+    }
+    steps {
+        dockerBuildAndPublish {
+			repositoryName('munawersheikh/docker-node-js-image-dsl')
+			registryCredentials('munawersheikh')
+			forcePull(false)
+			forceTag(false)
+			createFingerprints(false)
+			skipDecorate()
+		}
+    }
+}
